0cbb9ee1dfba3956e66dfd3cf5a3f87901bb04fd
SStrong01 <sierra.stancil01@gmail.com>
2025-04-30 18:30:26 -0400

    Fixed static folder case and updated style link

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mail import Mail, Message
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 # Mail configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
